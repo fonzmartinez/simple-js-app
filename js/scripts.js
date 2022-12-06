9bcc0fbfd1e197e6000e92fcc16c1686055e356a
fonzmartinez <119453742+fonzmartinez@users.noreply.github.com>
2022-12-06 16:24:39 -0600

    Added pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
-let pokemonList = []
+let pokemonList = [
+    {name: 'zubat', 
+    height: 0.8, 
+    type: ['poison', 'flying']
+    }, 
+    {name: 'slowpoke',
+    height: 1.2,
+    type: ['poison', 'flying']
+    }, 
+    {name: 'scrafty',
+    height: 1.1,
+    type: ['dark', 'fighting']
+    }
+]
