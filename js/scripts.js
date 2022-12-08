c5e0258cea1195e0498d9fcc7090db84545397a6
fonzmartinez <119453742+fonzmartinez@users.noreply.github.com>
2022-12-08 01:16:53 -0600

    Added breaks to conditonal

--- js/scripts.js
+++ js/scripts.js
@@ -15,11 +15,11 @@ let pokemonList = [
 // adding conditional for pokemon height
 for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height <=0.8) {
-        document.write(pokemonList[i].name + ' ' + pokemonList[i].height + ' is a tiny pokemon.');
+        document.write(pokemonList[i].name + ' ' + pokemonList[i].height + ' is a tiny pokemon.<br>');
     }
     else if (pokemonList[i].height >=.09 && pokemonList[i].height <=1.1) {
-        document.write(pokemonList[i].name + ' ' + pokemonList[i].height + ' is a average size pokemon.');
+        document.write(pokemonList[i].name + ' ' + pokemonList[i].height + ' is a average size pokemon.<br>');
     }
     else 
-        document.write(pokemonList[i].name + ' ' + pokemonList[i].height + ' is a large pokemon.');
+        document.write(pokemonList[i].name + ' ' + pokemonList[i].height + ' is a large pokemon.<br>');
 }
