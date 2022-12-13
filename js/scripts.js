1d84f3740a30fd1e922ffb576b35e8909563f472
fonzmartinez <119453742+fonzmartinez@users.noreply.github.com>
2022-12-12 23:58:02 -0600

    Updated with local variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,51 @@
-let pokemonList = [
-    {name: 'zubat', 
-    height: 0.8, 
-    type: ['poison', 'flying']
-    }, 
-    {name: 'slowpoke',
-    height: 1.2,
-    type: ['poison', 'flying']
-    }, 
-    {name: 'scrafty',
-    height: 1.1,
-    type: ['dark', 'fighting']
-    }
-]
+// adding IIFE
+
+let pokemonRepository = (function () {
+
+    let pokemonList = [
+        {name: 'zubat', 
+        height: 0.8, 
+        type: ['poison', 'flying']
+        }, 
+        {name: 'slowpoke',
+        height: 1.2,
+        type: ['poison', 'flying']
+        }, 
+        {name: 'scrafty',
+        height: 1.1,
+        type: ['dark', 'fighting']
+        }
+    ];
+
+        function getAll() {
+            return pokemonList;
+        }
+        function add(item) {
+            pokemonList.push(item);
+        } 
+
+        return {
+            getAll: getAll,
+            add: add 
+        }
+})(); 
+
+pokemonRepository.add({ name: 'Snorlax' });
+console.log(pokemonRepository.getAll());
+
+
 // adding conditional for pokemon height
 
-//for (let i = 0; i < pokemonList.length; i++) {
-   // if (pokemonList[i].height <=0.8) {
-   //     document.write(pokemonList[i].name + ' ' + pokemonList[i].height + ' is a tiny pokemon.<br>');
-   // }
-   // else if (pokemonList[i].height >=.09 && pokemonList[i].height <=1.1) {
-   //     document.write(pokemonList[i].name + ' ' + pokemonList[i].height + ' is a average size pokemon.<br>');
-   // }
-   // else 
-   //     document.write(pokemonList[i].name + ' ' + pokemonList[i].height + ' is a large pokemon.<br>');
-//}
+/* for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height <=0.8) {
+        document.write(pokemonList[i].name + ' ' + pokemonList[i].height + ' is a tiny pokemon.<br>');
+    }
+    else if (pokemonList[i].height >=.09 && pokemonList[i].height <=1.1) {
+        document.write(pokemonList[i].name + ' ' + pokemonList[i].height + ' is a average size pokemon.<br>');
+    }
+    else 
+        document.write(pokemonList[i].name + ' ' + pokemonList[i].height + ' is a large pokemon.<br>');
+} */
 
 // using forEach loop function
 
